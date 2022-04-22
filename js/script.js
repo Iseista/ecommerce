89498894bbaa1b35060862503addd4c107def7fa
Jerson Quintana <jersonscaner@gmail.com>
2022-04-22 18:08:08 -0500

    Add mi commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+//Seleccion icono busqueda
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = ()=>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+//Seleccion carro compra
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = ()=>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+//Seleccion login
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active')
+}
+
+//Seleccion menu
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = ()=>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active')
+}
+//----------------------------------------------------------------
+const contenedorIconos = document.querySelector("btn");
+const boton = document.querySelector("search-btn")
+
+boton.addEventListener("click", function(){
+    contenedorIconos.getElementsByClassName.color = "red";
+})
